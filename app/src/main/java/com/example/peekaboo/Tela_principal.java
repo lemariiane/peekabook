f31e2bedf91e0a5d56eda841a4909aa22154bfc4
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-11-01 23:34:08 -0300

    okok

--- app/src/main/java/com/example/peekaboo/Tela_principal.java
+++ app/src/main/java/com/example/peekaboo/Tela_principal.java
@@ -1,26 +1,59 @@
 package com.example.peekaboo;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.peekaboo.R;
+
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.example.peekaboo.databinding.ActivityTelaPrincipalBinding;
 
 public class Tela_principal extends AppCompatActivity {
+
+    ActivityTelaPrincipalBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_tela_principal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityTelaPrincipalBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new fragment_home());
 
 
+        binding.bottomNavigationView.setBackground(null);
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new fragment_home());
+                    break;
+
+                case R.id.curiosity:
+                    replaceFragment(new fragment_curiosity());
+                    break;
+
+                case R.id.pets:
+                    replaceFragment(new fragment_pets());
+                    break;
+
+                case R.id.profile:
+                    replaceFragment(new fragment_profile());
+                    break;
+            }
+
+            return true;
         });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
